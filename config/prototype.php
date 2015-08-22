d1b0af70f6782c74bb713b100a0dbc93fca61a7c
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-23 01:36:13 +0200

    Prototype config fragments are now their own file

---
+++ config/prototype.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ICanBoogie\Binding\CLDR;
+
+use ICanBoogie;
+
+$hooks = Hooks::class . '::';
+
+return [
+
+	ICanBoogie\Core::class . '::lazy_get_cldr_provider' => $hooks . 'get_cldr_provider',
+	ICanBoogie\Core::class . '::lazy_get_cldr' => $hooks . 'get_cldr',
+	ICanBoogie\Core::class . '::get_locale' => $hooks . 'get_locale',
+	ICanBoogie\Core::class . '::set_locale' => $hooks . 'set_locale'
+
+];
